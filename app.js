cd94da0347be16e998e3f3b5931624a6fe9a87ce
Martin Liu <liuzh66@gmail.com>
2023-02-03 22:32:33 +0800

    add from macos

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World insode docker; app v3.9!')
+  res.send('Hello World inside docker from macOS; !')
 })
 
 //Launch listening server on port 8080
